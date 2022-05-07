89aeb67068127f4ee7288e11f090682d4686c8c6
Chittaranjan <swainchittaranjan345@gmail.com>
2022-05-07 19:56:03 +0530

    This is before adding redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import './App.css';
-import Navbar from './component/Navbar';
-import Home from './component/Home';
+import "./App.css";
+import Navbar from "./component/Navbar";
+import Home from "./component/Home";
+import { Routes, Route } from "react-router-dom";
+import Product from "./component/Product";
+import ProductOne from "./component/ProductOne";
 
 function App() {
   return (
     <>
-       <Navbar/>
-       <Home/>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route exact path="/product" element={<Product/>} />
+        <Route exact path="/product/:id" element={<ProductOne/>} />
 
+      </Routes>
     </>
   );
 }
